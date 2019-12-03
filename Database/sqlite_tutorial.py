90db5beed7947bf7df6c0a40b16a74b4c1a806a4
dujiaxin <fashui01@163.com>
2019-12-03 15:37:21 -0500

    Feature: create customer table

--- Database/sqlite_tutorial.py
+++ Database/sqlite_tutorial.py
@@ -1,9 +1,45 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship,Session
-from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship,Session,sessionmaker
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
+    Column, DateTime, ForeignKey, Numeric, CheckConstraint
 
+from datetime import datetime
+
+metadata = MetaData()
 engine = create_engine("sqlite:///web/Sqlite-Data/example.db")
-session = Session(bind=engine)
+customers = Table('customers', metadata,
+                  Column('id', Integer(), primary_key=True),
+                  Column('first_name', String(100), nullable=False),
+                  Column('last_name', String(100), nullable=False),
+                  Column('username', String(50), nullable=False),
+                  Column('email', String(200), nullable=False),
+                  Column('address', String(200), nullable=False),
+                  Column('town', String(50), nullable=False),
+                  Column('created_on', DateTime(), default=datetime.now),
+                  Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+                  )
+
+items = Table('items', metadata,
+              Column('id', Integer(), primary_key=True),
+              Column('name', String(200), nullable=False),
+              Column('cost_price', Numeric(10, 2), nullable=False),
+              Column('selling_price', Numeric(10, 2), nullable=False),
+              Column('quantity', Integer(), nullable=False),
+              CheckConstraint('quantity > 0', name='quantity_check')
+              )
+
+orders = Table('orders', metadata,
+               Column('id', Integer(), primary_key=True),
+               Column('customer_id', ForeignKey('customers.id')),
+               Column('date_placed', DateTime(), default=datetime.now),
+               Column('date_shipped', DateTime())
+               )
+
+order_lines = Table('order_lines', metadata,
+                    Column('id', Integer(), primary_key=True),
+                    Column('order_id', ForeignKey('orders.id')),
+                    Column('item_id', ForeignKey('items.id')),
+                    Column('quantity', Integer())
+                    )
+
+metadata.create_all(engine)
