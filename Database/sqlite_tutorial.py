1c708774f20ef5df7408dcf893b81e2e0711e3d1
dujiaxin <fashui01@163.com>
2019-12-03 14:58:39 -0500

    Feature: create session

--- Database/sqlite_tutorial.py
+++ Database/sqlite_tutorial.py
@@ -1,3 +1,9 @@
-from sqlalchemy.orm import create_engine, Session
-engine = create_engine("postgres+psycopg2://postgres:pass@localhost/mydb")
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship,Session
+from sqlalchemy import create_engine
+
+engine = create_engine("sqlite:///web/Sqlite-Data/example.db")
 session = Session(bind=engine)
