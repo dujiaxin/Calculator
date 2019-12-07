4049eca1f59b47a7b94fc62483a90b67d11a0ffd
dujiaxin <fashui01@163.com>
2019-12-07 12:03:20 -0500

    Feature: section Updating data

--- Database/sqlite_tutorial.py
+++ Database/sqlite_tutorial.py
@@ -313,3 +313,15 @@ session.query(
 s1 = session.query(Item.id, Item.name).filter(Item.name.like("Wa%"))
 s2 = session.query(Item.id, Item.name).filter(Item.name.like("%e%"))
 s1.union(s2).all()
+
+s1.union_all(s2).all()
+
+# Updating Data
+i = session.query(Item).get(8)
+i.selling_price = 25.91
+session.add(i)
+session.commit()
+session.query(Item).filter(
+    Item.name.ilike("W%")
+).update({"quantity": 60}, synchronize_session='fetch')
+session.commit()
